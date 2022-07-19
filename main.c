50fa17e3e093338acf2d25da666e28955950a5a0
Matthew Hermary <matthermary@gmail.com>
2022-07-18 21:18:01 -0700

    updated to use input_pw_length

--- main.c
+++ main.c
@@ -18,28 +18,32 @@ int main()
     char input_pw[32];  //max size password of 32 characters
 
     while(fgets(input_pw, sizeof(input_pw), fp)) {      //Reads line by line from passwords.txt
+        unsigned int pw_length = strlen(password);      //Local variable for length to speed it up
+        unsigned int input_pw_length = strlen(input_pw) - 2;    //See how it is on ARM machines and adjust this
         printf("\nTrying out password: %s", input_pw);  //See which line is being read
-        printf("length is: %d\n", strlen(input_pw));    //Ensure proper length. This changes depending on which machine its running on for some reason.
-        for(int j = 0; j <= strlen(input_pw)-2; j++){   //For some reason reads two extra characters. Could be the '/0' to end the line in the txt file
-            printf("%c ", input_pw[j]);                 //Reads out character by character 
+        printf("length is: %d\n", input_pw_length);    //Ensure proper length. This changes depending on which machine its running on for some reason.
+        for(int j = 0; j <= input_pw_length; j++){   //For some reason reads two extra characters. Could be the '/0' to end the line in the txt file
+            printf(" %c", input_pw[j]);                 //Reads out character by character 
         }
         printf("\n");                                   //Newline for once the input password has been read out
-        unsigned int pw_length = strlen(password);      //Local variable for length to speed it up
-        if(strlen(input_pw)-1 != pw_length){            //See how it runs on ARM machines and adjust this
+        if(input_pw_length != pw_length){            //See how it runs on ARM machines and adjust this
             printf("ERROR - Password lengths did not match\n");
             //return 1;
         }
+        char nonce_pw[input_pw_length];
         unsigned int i;                                 //unsigned int since strlen returns unsigned int
-        for(i=0; i < pw_length; i++){
-            if(input_pw[i] != password[i]){
+        for(i=0; i < input_pw_length; i++){
+            nonce_pw[i] = password[i%pw_length];
+            if(input_pw[i] != nonce_pw[i]){
                 //return 1;
                 printf("ERROR - Password characters did not match\n");
-                printf("%c did not match %c\n", input_pw[i], password[i]);  //Character by character comparison
+                printf("%c did not match %c\n", input_pw[i], nonce_pw[i]);  //Character by character comparison
             }
             else{
-                printf("%c matched %c\n", input_pw[i], password[i]);        //So no characters are missed
+                printf("%c matched %c\n", input_pw[i], nonce_pw[i]);        //So no characters are missed
             }
         }
+        printf("Nonce PW was %s\n", nonce_pw);
     }
     fclose(fp);                                         //Close opened file
     return 0;
