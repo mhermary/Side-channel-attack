ac5d8e9e348f43b561edd0480990ba2d76f6ae3e
lab <matthewhermary@seng440.ece.uvic.ca>
2022-08-10 00:46:22 -0700

    Added ifdef for assembly code. Cleaned up passwords for presentation

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <stdbool.h>
 //#define TERMINAL_PRINT
 //#define CLOCKING
+//#define ASSEMBLY
 
 int main()
 {
@@ -20,9 +21,11 @@ int main()
 
     while(fgets(input_pw, sizeof(input_pw), fp))                //Reads line by line from passwords.txt
     {
+#ifdef ASSEMBLY
         __asm__ __volatile__(
             "@starting while loop here"
         );
+#endif
 #ifdef CLOCKING
         clock_t start_t, end_t;
         double total_t;
@@ -45,9 +48,11 @@ int main()
         }
         printf("\n");                                           //Newline for once the input password has been read out
 #endif
+#ifdef ASSEMBLY
         __asm__ __volatile__(
             "@comparing password lengths"
         );
+#endif
         if(input_pw_length != pw_length)                        //See how it runs on ARM machines and adjust this
         {
 #ifdef TERMINAL_PRINT
@@ -59,20 +64,24 @@ int main()
         {
             concat_pw_match = false;
         }
+#ifdef ASSEMBLY
         __asm__ __volatile__(
             "@creating concat_pw and inverse_concat_pw"
         );
+#endif
         char concat_pw[pw_length];
         char inverse_concat_pw[pw_length];
         register unsigned int i;                                //unsigned int since strlen returns unsigned int
         while(i < pw_length)
         {
+#ifdef ASSEMBLY
             __asm__ __volatile__(
                 "@starting letter by letter"
             );
+#endif
             concat_pw[i] = input_pw[i%input_pw_length];         //Fake password to compare so the real length isnt given away
             inverse_concat_pw[i] = ~input_pw[i%pw_length];
-            if(input_pw[i] != password[i])
+            if(concat_pw[i] != password[i])
             {
                 i++;        //incrementing i in all 3 if statements since it will only ever enter one
                             // this allows for pipelining for the second half of the loop
@@ -93,7 +102,7 @@ int main()
                     concat_pw_match = !concat_pw_match;         //Ensure a bool get toggled regardless
                 }
             }//if the character is incorrect
-            else if(input_pw[i] != inverse_concat_pw[i])        //bitwise inverted to consume same power 
+            else if(concat_pw[i] != inverse_concat_pw[i])        //bitwise inverted to consume same power 
             {
                 i++;
                 //just comparison for bit inversion to consume same power
@@ -119,7 +128,7 @@ int main()
             }
             concat_pw[i] = input_pw[i%input_pw_length];         //Fake password to compare so the real length isnt given away
             inverse_concat_pw[i] = ~input_pw[i%pw_length];
-            if(input_pw[i] != password[i])
+            if(concat_pw[i] != password[i])
             {
                 i++;
 #ifdef TERMINAL_PRINT
@@ -137,7 +146,7 @@ int main()
                     concat_pw_match = !concat_pw_match;         //Ensure bool toggle
                 }
             }//if the character is incorrect
-            else if(input_pw[i] != inverse_concat_pw[i])        //bitwise inverted to consume same power 
+            else if(concat_pw[i] != inverse_concat_pw[i])        //bitwise inverted to consume same power 
             {
                 i++;
                 //just comparison for bit inversion to consume same power
@@ -169,9 +178,11 @@ int main()
         printf("Took %ld to %ld cycles to try %s", start_t, end_t, input_pw);
         printf("Total time taken by CPU: %f\n", total_t  );
 #endif
+#ifdef ASSEMBLY
         __asm__ __volatile__(
             "@checking if passwords match"
         );
+#endif
         if(pw_match)
         {
             printf("Password match: %s", input_pw);
